7b6d24aae9d6d0d0fca836173b3037743c67f76b
Viacheslav Kravchenko <edocsil@knu.ua>
2019-03-19 13:56:14 +0200

    some changes

--- lab_1/entropy.cs
+++ lab_1/entropy.cs
@@ -11,11 +11,30 @@ namespace lab_1
         static void Main(string[] args)
         {
             string dir_address = @"D:\UNIV\Comp_Sys\lab_1\";
-            string[] file_name = new string[] { "scp_1.txt", "brake_2.txt", "rick_3.txt" };
+            //string[] file_name = new string[] { "scp_1.txt", "brake_2.txt", "rick_3.txt" }; //original text files
+            //string[] file_name = new string[] { "1.enc", "2.enc", "3.enc" }; //base64 ecrypted original files
+            //string[] file_name = new string[] { "scp_1.txt.bz2", "brake_2.txt.bz2", "rick_3.txt.bz2" }; //original f. after bzip2
+            string[] file_name = new string[] { "1b.enc", "2b.enc", "3b.enc" };// base64 enc. bzip2 files
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            char[] alphabet = new char[33] {'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й',
-                    'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
-                    'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я' };
+
+            //alphabet fro Ukrainian, Digits and Symbols, which are used in texts
+            //char[] alphabet = new char[84] {'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й',
+            //        'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+            //        'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я',
+            //    'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й',
+            //        'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
+            //        'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я',
+            //    ',',' ','(', ')', '.', ':', '-','"',
+            //    '0','1','2','3','4','5','6','7','8','9'
+            //};
+
+            //alphabet for Base64
+            char[] alphabet = new char[64]
+               {  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+            'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m',
+            'n','o','p','q','r','s','t','u','v','w','x','y','z',
+            '0','1','2','3','4','5','6','7','8','9','+','/'};
 
             for (int f = 0; f < file_name.Length; f++)
             {
@@ -39,7 +58,7 @@ namespace lab_1
                                 var count = line.Count(x => x == alphabet[i]);
                                 numberOfOccurence[i] += count;
                             }
-                            amount_of_letters += line.Count(Char.IsLetter);
+                            amount_of_letters += line.Count();
                         }
                     }
                     Console.WriteLine("Загальна кількість символів файлу: " + amount_of_letters);
