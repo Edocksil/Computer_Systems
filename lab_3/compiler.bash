0972a9c3e49a302c048c29cb4c85fa0eb225f184
Viacheslav Kravchenko <edocsil@knu.ua>
2019-04-15 01:21:04 +0300

    some changes

--- lab_3/compiler.bash
+++ lab_3/compiler.bash
@@ -1,6 +1,6 @@
 #!/bin/bash                                                                                                                                                           
 flags=`grep ^flags /proc/cpuinfo | uniq | cut -d':' -f2 | cut -d" " -f2- | tr _ . | tr "a-z" "A-Z"`
-                                                                                                                                                                        
+ml icc                                                                     
         for flag in $flags; do
                                                                                                                                                                         
                 icc -O1 -x$flag fibo.cpp -o test.out 2> error
@@ -8,9 +8,9 @@ flags=`grep ^flags /proc/cpuinfo | uniq | cut -d':' -f2 | cut -d" " -f2- | tr _ 
                 if [ ! -s "error" ]; then #check if error occured - so the flag is compatible   
                                                                                                                                                                         
                         for o in {1..3}; do
-                                echo "---------------------------" >> statistic.txt
-                                echo "$flag with -O$o"             >> statistic.txt
-                                time `icc -O$o -x$flag fibo.cpp -o $flag_o$o.out`  >> statistic.txt
+                                echo "---------------------------" 
+                                echo "$flag with -O$o"             
+                                time `icc -O$o -x$flag fibo.cpp -o $flag_o$o.out` 
                         done
                 fi
         done
--- lab_3/timer.bash
+++ lab_3/timer.bash
@@ -1,9 +1,8 @@
 #!/bin/bash    
-                                                                                                             
-for file in `ls ./`                                                                                                                                        
-do                                                                                                                                                   
-	echo $file                                                                                                                                              
-	time `for i in {1..10}; do
-		./file
-	done`
+ml icc                                                                                 
+for file in `find ./ -type  f -name '*\.out'`                                                                                                                                        
+do        
+	echo "*******************************"																																		
+	echo "$file for 10 times executed in"                                                                                                                                               
+	time `for i in {1..10}; do ./file; done`
 done                                                                                                                                                 
