a426913ed664a1843ab6e7aac879c09cc7b53a7d
Viacheslav Kravchenko <edocsil@knu.ua>
2019-04-15 02:25:14 +0300

    maagic

--- lab_3/script.sh
+++ lab_3/script.sh
@@ -1,16 +1,18 @@
 #!/bin/bash                                                                                                                                                           
-flags=`grep ^flags /proc/cpuinfo | uniq | cut -d':' -f2 | cut -d" " -f2- | tr _ . | tr "a-z" "A-Z"`                                                                                                                                                   
-ml icc                                                                                                                                                                                                                                       
-        for flag in $flags; do
-                icc -O1 -x$flag fibo.cpp -o test.out 2> error                                                                                                                                          
-                if [ ! -s "error" ]; then #check if error occured - so the flag is compatible                                                                                                              
-                        for o in {1..3}; do
-                                echo "---------------------------" 
-                                echo "$flag with -O$o"             
-                                time `icc -O$o -x$flag fibo.cpp`
-                                echo "*******************************"
-                                echo "file executed in"
-                                time `./a.out`
-                        done
-                fi
-        done
+	flags=`grep ^flags /proc/cpuinfo | uniq | cut -d':' -f2 | cut -d" " -f2- | tr _ . | tr "a-z" "A-Z"`                                                                                                                                                   
+	ml icc
+		for flag in $flags; do
+			icc -O1 -x$flag fibo.cpp -o test.out 2> error
+				if [ ! -s "error" ]; then #check if error occured - so the flag is compatible
+					for o in {1..3}; do
+						echo "---------------------------" 
+						echo "$flag with -O$o"             
+						time `icc -O$o -x$flag fibo.cpp`
+						echo "*******************************"
+						echo "file executed for 5 times in"
+							for i in {1..5}; do
+								time `./a.out`
+							done
+					done
+				fi
+		done;
