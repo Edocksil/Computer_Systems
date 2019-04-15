c391de6fca715dc096635d5c3d1270dfbe32b2ba
Viacheslav Kravchenko <edocsil@knu.ua>
2019-04-15 22:58:46 +0300

    removed extra

--- lab_3/timer.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash    
-ml icc                                                                                 
-for file in `find ./ -type  f -name '*\.out'`                                                                                                                                        
-do        
-	echo "*******************************"																																		
-	echo "$file for 10 times executed in"                                                                                                                                               
-	time `for i in {1..10}; do ./file; done`
-done                                                                                                                                                 
